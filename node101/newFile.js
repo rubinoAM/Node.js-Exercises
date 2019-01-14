38b1cd459facf4ad32670c44fe0dc7581431ee3e
rubinoAM <parkcathedralgrace@gmail.com>
2019-01-14 10:26:48 -0500

    newFile.js

---
+++ node101/newFile.js
@@ -0,0 +1,12 @@
+const x = 1;
+console.log(x);
+let atlTeams = {
+    'Braves':'Baseball',
+    'Falcons':'American Football',
+    'Hawks':'Basketball',
+    'United':'Football'};
+console.log(atlTeams);
+
+// Require in node.js is equivalent to "from * import *" in Python
+const http = require('http');
+console.log(http);
